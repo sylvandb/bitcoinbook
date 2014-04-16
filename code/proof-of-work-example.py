8f25fd114b841950e89c2a4de3c749c86fe2f19f
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2014-04-15 21:04:58 -0600

    structure for random nonces

--- code/proof-of-work-example.py
+++ code/proof-of-work-example.py
@@ -1,48 +1,63 @@
 #!/usr/bin/env python
 # example of proof-of-work algorithm
 
-import hashlib
-import time
+from hashlib import sha256
+from time import time
+from random import randint,seed
 
 max_nonce = 2 ** 32 # 4 billion
 
-def proof_of_work(header, difficulty_bits):
-    
-    target = 2 ** (256-difficulty_bits)
-    
-    for nonce in xrange(max_nonce):
-        hash_result = hashlib.sha256(str(header)+str(nonce)).hexdigest()
-        
-        if long(hash_result, 16) < target:
-            print "Success with nonce %d" % nonce
-            print "Hash is %s" % hash_result
-            return (hash_result,nonce)
-            
-    print "Failed after %d (max_nonce) tries" % nonce
-    return nonce
 
-    
+def proof_of_work(header, difficulty_bits):
+
+    target = 2 ** (256-difficulty_bits)
+    header = str(header)
+
+    # for random nonce's, replace the "nonce = ..." line with:
+        #nonce = randint(0, max_nonce)
+    # use ^^^ to replace the line "nonce = ..." inside the loop
+    # (putting a comment inside the loop slows it down)
+    for tries in xrange(max_nonce):
+        nonce = tries
+        hash_result = sha256(header+str(nonce)).hexdigest()
+
+        if long(hash_result, 16) < target:
+            print "Success with nonce %s" % nonce
+            return (hash_result,tries)
+
+    return ('',tries)
+
+
 if __name__ == '__main__':
     
     nonce = 0
     hash_result = ''
+    seed(42)
         
     for difficulty_bits in xrange(32):
         
         difficulty = 2 ** difficulty_bits
-        print "Difficulty: %ld (%d bits)" % (difficulty, difficulty_bits)
+        print "\nDifficulty: %ld (%d bits)" % (difficulty, difficulty_bits)
     
         print "Starting search..."
-        start_time = time.time()
+        start_time = time()
         new_block = 'test block with transactions' + hash_result # make a new block which includes the hash from the previous block
-        (hash_result, nonce) = proof_of_work(new_block, difficulty_bits) # find a nonce for the new block
-        end_time = time.time()
+        (hash_result, tries) = proof_of_work(new_block, difficulty_bits) # find a nonce for the new block
+        end_time = time()
+        if hash_result:
+            print "Hash is %s" % hash_result
+        else:
+            print "Failed after %d tries" % tries
         elapsed_time = end_time - start_time
         print "Elapsed Time: %.4f seconds" % elapsed_time
     
         if elapsed_time > 0:
-            hash_power = float(long(nonce)/elapsed_time)
+            hash_power = float(long(tries)/elapsed_time)
             print "Hashing Power: %ld hashes per second" % hash_power
+
+        if not hash_result:
+            print "Giving up."
+            break
     
     
     
